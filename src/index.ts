17cd31b122f10df727d637bac1d8a2e83eec0b70
Jintao Koong <jintaokoong99@gmail.com>
2022-01-08 18:25:51 +0800

    ♻️ reorganise index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,155 +1,19 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { addDays, isBefore, parse } from 'date-fns';
-import { format, utcToZonedTime } from 'date-fns-tz';
-import express, { json, Router } from 'express';
-import lodash from 'lodash';
-import * as zmq from 'zeromq';
+import express, { json } from 'express';
 import { connect } from 'mongoose';
 import { Config } from './interfaces/config';
-import { SongRequest } from './interfaces/song-request';
-import srMapper from './mapper/sr-mapper';
-import objectUtils from './utils/object-utils';
+import configRouter from './routers/config-router';
+import requestRouter from './routers/request-router';
+import zmqSocket from './servers/zmq-socket';
 
 dotenv.config();
 const dbHost = process.env.DB_HOST ?? 'localhost';
-
-const sock = new zmq.Push();
 
 const app = express();
 
 app.use(cors());
 app.use(json());
-
-const requestRouter = Router();
-requestRouter.use(async (req, res, next) => {
-  if (req.method !== 'POST') {
-    return next();
-  }
-  const accepting = await Config.findOne({ name: 'accepting' });
-  if (accepting && accepting.value === 'true') {
-    return next();
-  }
-  return res.status(400).send({ message: 'currently not accpeting' });
-});
-requestRouter.delete('/request/:id', async (req, res) => {
-  if (!req.params.id) {
-    return res.status(404).send();
-  }
-  const doc = await SongRequest.findOne({ _id: req.params.id });
-  if (!doc) {
-    return res.status(404).send();
-  }
-
-  try {
-    await doc.remove();
-    return res.status(200).send();
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send();
-  }
-});
-requestRouter.get('/request', async (_, res) => {
-  const lastWeek = addDays(new Date(), -7);
-  const docs = await SongRequest.find({ createdAt: { $gte: lastWeek } });
-  const objs = docs.map((d) => d.toObject());
-  const grouped = lodash.groupBy(objs, (o) => {
-    const time = utcToZonedTime(o.createdAt, 'Asia/Kuala_Lumpur');
-    return format(time, 'yyyy-MM-dd');
-  });
-  const mapped = objectUtils
-    .getKeys(grouped)
-    .sort((a, b) => {
-      const f = parse(a as string, 'yyyy-MM-dd', new Date());
-      const s = parse(b as string, 'yyyy-MM-dd', new Date());
-      return isBefore(f, s) ? 1 : -1;
-    })
-    .reduce(
-      (pv, cv) => ({
-        ...pv,
-        [cv]: grouped[cv].map((sr) => srMapper.map(sr)),
-      }),
-      {}
-    );
-  return res.send(mapped);
-});
-
-requestRouter.post('/request', async (req, res) => {
-  const { name } = req.body;
-  const errors = [];
-  if (name === undefined) {
-    errors.push('name is undefined');
-  }
-  if (errors.length > 0) {
-    return res.status(400).send({
-      errors: errors,
-    });
-  }
-
-  const time = utcToZonedTime(new Date(), 'Asia/Kuala_Lumpur');
-  const key = format(time, 'yyyy-MM-dd');
-  const doc = new SongRequest({
-    name: name,
-    done: false,
-    key: key,
-  });
-  const out = await doc.save();
-  const payload = [out._id, name];
-  sock.send(payload.join(','));
-  return res.send(srMapper.map(out.toObject()));
-});
-
-requestRouter.put('/request/:id', async (req, res) => {
-  let doc;
-  try {
-    doc = await SongRequest.findById(req.params.id);
-  } catch (err) {
-    return res.status(404).send();
-  }
-  if (!doc) {
-    return res.status(404).send();
-  }
-
-  if (req.body.done !== undefined) {
-    doc.done = req.body.done;
-  }
-
-  if (req.body.details !== undefined) {
-    doc.details = req.body.details;
-  }
-
-  try {
-    const result = await doc.save();
-    return res.status(200).send(srMapper.map(result.toObject()));
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send();
-  }
-});
-
-const configRouter = Router();
-
-configRouter.get('/config', async (_, res) => {
-  const accepting = await Config.findOne({ name: 'accepting' });
-  return res.send({
-    accepting: accepting?.value,
-  });
-});
-configRouter.post('/config/toggle', async (_, res) => {
-  const accepting = await Config.findOne({ name: 'accepting' });
-  if (!accepting) {
-    const doc = new Config({
-      name: 'accepting',
-      value: 'false',
-    });
-    return res.status(200).send();
-  }
-  accepting.value = accepting.value === 'true' ? 'false' : 'true';
-  await accepting.save();
-  return res.send({
-    accepting: accepting.value,
-  });
-});
 
 app.get('/', (_, res) => res.status(200).send({ message: 'server is up!' }));
 app.use('/api', configRouter, requestRouter);
@@ -167,7 +31,7 @@ connect(`mongodb://srapp:XgKaZ3SE8Ctvc5KF4nqc@${dbHost}/ddsrdb`)
     }
   })
   .then(() =>
-    sock.bind('tcp://127.0.0.1:4200').then(() => {
+    zmqSocket.bind('tcp://127.0.0.1:4200').then(() => {
       console.log('bound to port 4200');
       return app.listen(4000, () => console.log('listening on port 4000'));
     })
---
+++ src/routers/request-router.ts
@@ -0,0 +1,120 @@
+import { addDays, isBefore, parse } from 'date-fns';
+import { format, utcToZonedTime } from 'date-fns-tz';
+import { Router } from 'express';
+import lodash from 'lodash';
+import { Config } from '../interfaces/config';
+import { SongRequest } from '../interfaces/song-request';
+import srMapper from '../mapper/sr-mapper';
+import zmqSocket from '../servers/zmq-socket';
+import objectUtils from '../utils/object-utils';
+
+const requestRouter = Router();
+
+requestRouter.use(async (req, res, next) => {
+  if (req.method !== 'POST') {
+    return next();
+  }
+  const accepting = await Config.findOne({ name: 'accepting' });
+  if (accepting && accepting.value === 'true') {
+    return next();
+  }
+  return res.status(400).send({ message: 'currently not accpeting' });
+});
+
+requestRouter.delete('/request/:id', async (req, res) => {
+  if (!req.params.id) {
+    return res.status(404).send();
+  }
+  const doc = await SongRequest.findOne({ _id: req.params.id });
+  if (!doc) {
+    return res.status(404).send();
+  }
+
+  try {
+    await doc.remove();
+    return res.status(200).send();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send();
+  }
+});
+
+requestRouter.get('/request', async (_, res) => {
+  const lastWeek = addDays(new Date(), -7);
+  const docs = await SongRequest.find({ createdAt: { $gte: lastWeek } });
+  const objs = docs.map((d) => d.toObject());
+  const grouped = lodash.groupBy(objs, (o) => {
+    const time = utcToZonedTime(o.createdAt, 'Asia/Kuala_Lumpur');
+    return format(time, 'yyyy-MM-dd');
+  });
+  const mapped = objectUtils
+    .getKeys(grouped)
+    .sort((a, b) => {
+      const f = parse(a as string, 'yyyy-MM-dd', new Date());
+      const s = parse(b as string, 'yyyy-MM-dd', new Date());
+      return isBefore(f, s) ? 1 : -1;
+    })
+    .reduce(
+      (pv, cv) => ({
+        ...pv,
+        [cv]: grouped[cv].map((sr) => srMapper.map(sr)),
+      }),
+      {}
+    );
+  return res.send(mapped);
+});
+
+requestRouter.post('/request', async (req, res) => {
+  const { name } = req.body;
+  const errors = [];
+  if (name === undefined) {
+    errors.push('name is undefined');
+  }
+  if (errors.length > 0) {
+    return res.status(400).send({
+      errors: errors,
+    });
+  }
+
+  const time = utcToZonedTime(new Date(), 'Asia/Kuala_Lumpur');
+  const key = format(time, 'yyyy-MM-dd');
+  const doc = new SongRequest({
+    name: name,
+    done: false,
+    key: key,
+  });
+  const out = await doc.save();
+  const payload = [out._id, name];
+  zmqSocket.send(payload.join(','));
+  return res.send(srMapper.map(out.toObject()));
+});
+
+requestRouter.put('/request/:id', async (req, res) => {
+  let doc;
+  try {
+    doc = await SongRequest.findById(req.params.id);
+  } catch (err) {
+    return res.status(404).send();
+  }
+  if (!doc) {
+    return res.status(404).send();
+  }
+
+  if (req.body.done !== undefined) {
+    doc.done = req.body.done;
+  }
+
+  if (req.body.details !== undefined) {
+    doc.details = req.body.details;
+  }
+
+  try {
+    const result = await doc.save();
+    return res.status(200).send(srMapper.map(result.toObject()));
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send();
+  }
+});
+
+export default requestRouter;
